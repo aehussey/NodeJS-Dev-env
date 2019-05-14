84c28bddfed0c57916ab07d4f802b23d69584564
Tech-A06 <tech-a06@Tech-A06s-MacBook-Air.local>
2019-05-14 10:40:21 +0100

    Added install of express, mongoose and ejs in provision

--- dev-environments/environment/provision.sh
+++ dev-environments/environment/provision.sh
@@ -5,8 +5,13 @@ sudo apt-get upgrade -y
 sudo apt-get install nginx -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
+
+
 npm install pm2 -g
 sudo systemctl start nginx
+npm install express
+npm install mongoose
+npm install ejs
+
 cd /app
-npm install -g
 pm2 start app.js
