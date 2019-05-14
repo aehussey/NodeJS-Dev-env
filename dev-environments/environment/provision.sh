b136068833dc9109c1cfaa95102b333f84e9010b
Tech-A06 <tech-a06@Tech-A06s-MacBook-Air.local>
2019-05-14 11:04:00 +0100

    Added a global npm installer

--- dev-environments/environment/provision.sh
+++ dev-environments/environment/provision.sh
@@ -5,13 +5,10 @@ sudo apt-get upgrade -y
 sudo apt-get install nginx -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
-
-
 npm install pm2 -g
 sudo systemctl start nginx
 npm install express
 npm install mongoose
 npm install ejs
-
 cd /app
 pm2 start app.js
